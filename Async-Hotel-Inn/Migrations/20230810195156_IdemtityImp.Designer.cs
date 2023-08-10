b5c5fb8841d1ed6754bcd3f549b72a1ff028d330
Jared E Plummer <113934345+JaredPlummer5@users.noreply.github.com>
2023-08-10 15:16:25 -0500

    Swagger

--- Async-Hotel-Inn/Migrations/20230810195156_IdemtityImp.Designer.cs
+++ Async-Hotel-Inn/Migrations/20230810195156_IdemtityImp.Designer.cs
@@ -4,6 +4,7 @@ using Async_Hotel_Inn.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Async_Hotel_Inn.Migrations
 {
     [DbContext(typeof(AsyncInnContext))]
-    partial class AsyncInnContextModelSnapshot : ModelSnapshot
+    [Migration("20230810195156_IdemtityImp")]
+    partial class IdemtityImp
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
